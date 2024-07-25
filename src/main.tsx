c4968ad6c5ab1e8222faedec459024d107ea76dc
Antoine Barbier <antoine.c.barbier@frog.co>
2024-07-26 00:44:05 +0200

    refactor: rename app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./app";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <App />
-  </>
+  <App />
 );
