ef6e6718aee35c16c000c4b1fb7f6960bd8871e5
Julien Boulvert <julien.boulvert@frog.co>
2024-08-28 22:11:27 +0200

    feat: add error toast

---
+++ src-tauri/src/errors.rs
@@ -0,0 +1,40 @@
+use serde::Serialize;
+use tauri::{Runtime, Window, Result};
+
+#[derive(Clone, Serialize, Debug, Default)]
+pub struct ErrorPayload {
+    pub title: Option<String>,
+    pub message: Option<String>,
+}
+
+impl ErrorPayload {
+    pub(crate) fn new() -> Self {
+        Self::default()
+    }
+
+    pub(crate) fn with_title(mut self, title: impl Into<String>) -> Self {
+        self.title = Some(title.into());
+        self
+    }
+
+    pub(crate) fn with_message(mut self, message: impl Into<String>) -> Self {
+        self.message = Some(message.into());
+        self
+    }
+}
+
+pub(crate) trait EmitError {
+    fn emit_error(
+        &self,
+        error: ErrorPayload
+    ) -> Result<()>;
+}
+
+impl<R: Runtime> EmitError for Window<R> {
+    fn emit_error(&self, error: ErrorPayload) -> Result<()>{
+        self.emit(
+            "error",
+            error
+        )
+    }
+}
