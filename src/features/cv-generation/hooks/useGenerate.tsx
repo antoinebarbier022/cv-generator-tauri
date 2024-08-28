3139160bef1b2f6cf7e1c4caa7d0ef9b1f886b92
Antoine Barbier <antoine.c.barbier@frog.co>
2024-08-29 01:37:05 +0200

    fix(toast generation success): change design

--- src/features/cv-generation/hooks/useGenerate.tsx
+++ src/features/cv-generation/hooks/useGenerate.tsx
@@ -1,4 +1,5 @@
-import { Link, Stack, Typography } from "@mui/joy";
+import { CloseRounded } from "@mui/icons-material";
+import { IconButton, Link, Stack, Typography } from "@mui/joy";
 import { useMutation } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/tauri";
 import { toast } from "react-toastify";
@@ -14,34 +15,54 @@ export const useGenerate = () => {
         const fileName = outputList[outputList.length - 1];
 
         const renderContent = (
-          <Stack>
-            <Typography textColor={"text.primary"}>{fileName}</Typography>
-            <Typography
-              className="group-hover:hidden"
-              sx={{ width: "fit-content" }}
-              fontSize={"0.75rem"}
-            >
-              PPTX
-            </Typography>
+          <Stack
+            direction={"row"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+          >
+            <Stack>
+              <Typography textColor={"text.primary"}>{fileName}</Typography>
+              <Stack direction={"column-reverse"}>
+                <Link
+                  className="peer"
+                  mt={1}
+                  sx={{ width: "fit-content" }}
+                  fontSize={"0.75rem"}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    invoke("open_finder", { path: outputFilePath });
+                    toast.dismiss("generation-succeeded");
+                  }}
+                >
+                  Afficher dans le finder
+                </Link>
+                <Typography
+                  className="group-hover:hidden peer-hover:flex group-has-[.close:hover]:flex"
+                  sx={{ width: "fit-content" }}
+                  fontSize={"0.75rem"}
+                >
+                  PPTX
+                </Typography>
 
-            <Typography
-              sx={{ width: "fit-content", display: "none" }}
-              className="group-hover:flex"
-              fontSize={"0.75rem"}
-            >
-              Ouvrir dans Microsoft PowerPoint
-            </Typography>
-
-            <Link
-              sx={{ width: "fit-content" }}
-              fontSize={"0.75rem"}
+                <Typography
+                  sx={{ width: "fit-content", display: "none" }}
+                  className="group-hover:flex peer-hover:hidden group-has-[.close:hover]:hidden"
+                  fontSize={"0.75rem"}
+                >
+                  Ouvrir dans Microsoft PowerPoint
+                </Typography>
+              </Stack>
+            </Stack>
+            <IconButton
+              size="sm"
+              className="close"
               onClick={(e) => {
                 e.stopPropagation();
-                invoke("open_finder", { path: outputFilePath });
+                toast.dismiss("generation-succeeded");
               }}
             >
-              Afficher dans le finder
-            </Link>
+              <CloseRounded />
+            </IconButton>
           </Stack>
         );
 
@@ -54,8 +75,10 @@ export const useGenerate = () => {
           pauseOnHover: true,
           pauseOnFocusLoss: true,
           progress: undefined,
+          closeButton: false,
           theme: "light",
           className: "group",
+
           onClick(e) {
             e.stopPropagation();
             invoke("open_powerpoint", { path: outputFilePath });
