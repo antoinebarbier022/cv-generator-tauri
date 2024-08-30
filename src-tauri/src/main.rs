97db6d2350abe512ad70b61217b70523d1f7ab86
Antoine Barbier <antoine.c.barbier@frog.co>
2024-08-30 03:54:35 +0200

    fix: shutdown server on quit, kill, termination
    * feat: show error toast when backend start command failed
    
    * feat: try to kill api server on exit
    
    * fix: shutdown server on quit, kill, termination
    
    ---------
    
    Co-authored-by: Julien Boulvert <julien.boulvert@frog.co>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,45 +3,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod errors;
 mod commands;
+mod errors;
 mod menu;
 
+use crate::commands::{get_backend_error, open_finder, open_powerpoint};
+use crate::errors::{EmitError, ErrorPayload};
+use crate::menu::{create_app_menu, MyMenu};
+use anyhow::Context;
+use core::str;
+use libc::SIGTERM;
+use serde::Serialize;
+use signal_hook::iterator::Signals;
 use std::convert::identity;
-use std::sync::mpsc::{channel, RecvError, Sender};
 use std::sync::Mutex;
 use std::thread;
-use tauri::api::process::{Command, CommandChild, CommandEvent, TerminatedPayload};
+use tauri::api::process::{Command, CommandEvent, TerminatedPayload};
 use tauri::api::shell::open;
-use tauri::{Manager, WindowEvent};
+use tauri::Manager;
 use window_vibrancy::*;
-use crate::commands::{get_backend_error, open_finder, open_powerpoint};
-use crate::menu::{create_app_menu, MyMenu};
-use crate::errors::{EmitError, ErrorPayload};
-use anyhow::Context;
-use serde::Serialize;
 
-/// Start the api server
-/// Returns a channel Sender used to trigger the process kill
-fn start_backend() -> Result<Sender<()>, anyhow::Error> {
-    let (tx_kill, rx_kill) = channel();
+fn start_backend() -> anyhow::Result<()> {
+    println!("Spawning api server...");
+    spawn_backend()?;
 
-    let child = spawn_backend()?;
-
-    thread::spawn(move || {
-        match rx_kill.recv() {
-            Ok(()) => println!("Received kill signal!"),
-            Err(RecvError) => println!("Kill channel was closed!"),
-        }
-        println!("Closing `api` sidecar...");
-        child.kill().expect("killing api server process.");
-    });
-
-    Ok(tx_kill)
+    Ok(())
 }
 
-fn spawn_backend() -> anyhow::Result<CommandChild> {
-    let (mut rx, child) = Command::new_sidecar("api")
+fn spawn_backend() -> anyhow::Result<()> {
+    let (mut rx, _) = Command::new_sidecar("api")
         .context("Failed to create `api` binary command")?
         .spawn()
         .context("Failed to spawn `api` sidecar")?;
@@ -52,14 +42,15 @@ fn spawn_backend() -> anyhow::Result<CommandChild> {
                 CommandEvent::Stderr(line) => print!("[API-LOG]: {line}"),
                 CommandEvent::Stdout(line) => print!("[API]: {line}"),
                 CommandEvent::Error(err) => println!("Error listening to api {err}"),
-                CommandEvent::Terminated(TerminatedPayload { code, signal}) => {
+                CommandEvent::Terminated(TerminatedPayload { code, signal }) => {
                     let infos = vec![
                         code.map(|code| format!("code {code}")),
                         signal.map(|signal| format!("signal {signal}")),
-                    ].into_iter()
-                        .filter_map(identity)
-                        .collect::<Vec<_>>()
-                        .join(" and ");
+                    ]
+                    .into_iter()
+                    .filter_map(identity)
+                    .collect::<Vec<_>>()
+                    .join(" and ");
 
                     if !infos.trim().is_empty() {
                         println!("Api terminated.")
@@ -74,15 +65,39 @@ fn spawn_backend() -> anyhow::Result<CommandChild> {
         }
     });
 
-    Ok(child)
+    Ok(())
 }
 
 #[derive(Default, Debug, Serialize)]
 struct AppState {
-    pub backend_error: Mutex<Option<String>>
+    pub backend_error: Mutex<Option<String>>,
 }
 
-fn main() -> tauri::Result<()> {
+extern "C" fn on_exit() {
+    println!("Application exited.\nClosing `api` sidecar...");
+
+    let response = std::process::Command::new("curl")
+        .args(vec!["-X", "POST", "http://localhost:8008/api/v1/shutdown"])
+        .output()
+        .unwrap()
+        .stdout;
+    let response = str::from_utf8(&response).unwrap();
+
+    println!("Server shutdown : {response}");
+}
+
+fn main() -> anyhow::Result<()> {
+    unsafe {
+        libc::atexit(on_exit);
+    }
+    let mut signals = Signals::new([SIGTERM])?;
+
+    thread::spawn(move || {
+        for _sig in signals.forever() {
+            on_exit();
+        }
+    });
+
     tauri::Builder::default()
         .manage(AppState::default())
         .menu(create_app_menu())
@@ -92,22 +107,43 @@ fn main() -> tauri::Result<()> {
                 Some(MyMenu::FileExport) => event.window().emit("file-export", "").unwrap(),
                 Some(MyMenu::FileReset) => event.window().emit("file-reset", "").unwrap(),
                 Some(MyMenu::FileGenerate) => event.window().emit("file-generate", "").unwrap(),
-                Some(MyMenu::FileGenerateAndSaveAs) => event.window().emit("file-generate-and-save-as", "").unwrap(),
-                Some(MyMenu::AppPreferences) => { todo!() }
-                Some(MyMenu::AppUpdate) => { todo!() }
-                Some(MyMenu::DebugOpenPanel) => event.window().emit("debug-open-panel", "").unwrap(),
-                Some(MyMenu::DebugSendError) => event.window().emit_error(
-                    ErrorPayload::new()
-                        .with_title("Ceci est un titre")
-                        .with_message("Ceci est un message")
-                ).unwrap(),
-                Some(MyMenu::HelpOpenSlack) => if let Err(err) = open(
-                    &event.window().shell_scope(),
-                    "https://capgemini.enterprise.slack.com/archives/C07DCNBUT4Z",
-                    None,
-                ) {
-                    event.window().emit_error(ErrorPayload::new().with_title("Failed to open Slack channel").with_message(err.to_string())).unwrap();
-                },
+                Some(MyMenu::FileGenerateAndSaveAs) => event
+                    .window()
+                    .emit("file-generate-and-save-as", "")
+                    .unwrap(),
+                Some(MyMenu::AppPreferences) => {
+                    todo!()
+                }
+                Some(MyMenu::AppUpdate) => {
+                    todo!()
+                }
+                Some(MyMenu::DebugOpenPanel) => {
+                    event.window().emit("debug-open-panel", "").unwrap()
+                }
+                Some(MyMenu::DebugSendError) => event
+                    .window()
+                    .emit_error(
+                        ErrorPayload::new()
+                            .with_title("Ceci est un titre")
+                            .with_message("Ceci est un message"),
+                    )
+                    .unwrap(),
+                Some(MyMenu::HelpOpenSlack) => {
+                    if let Err(err) = open(
+                        &event.window().shell_scope(),
+                        "https://capgemini.enterprise.slack.com/archives/C07DCNBUT4Z",
+                        None,
+                    ) {
+                        event
+                            .window()
+                            .emit_error(
+                                ErrorPayload::new()
+                                    .with_title("Failed to open Slack channel")
+                                    .with_message(err.to_string()),
+                            )
+                            .unwrap();
+                    }
+                }
                 None => { /* do nothing */ }
             };
         })
@@ -122,28 +158,18 @@ fn main() -> tauri::Result<()> {
             apply_blur(&window, Some((18, 18, 18, 125)))
                 .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
-            let tx_kill= match start_backend() {
-                Ok(tx_kill) => {
-                    *app.state::<AppState>().backend_error.lock().unwrap() = None;
-                    Some(tx_kill)
-                },
-                Err(err) => {
-                    println!("Failed to start backend: {err}");
-                    *app.state::<AppState>().backend_error.lock().unwrap() = Some(err.to_string());
-                    None
-                }
-            };
-
-            window.on_window_event(move |event| {
-                if let WindowEvent::Destroyed = event && let Some(tx_kill) = &tx_kill {
-                    println!("[Event] App closed, shutting down API...");
-                    tx_kill.send(()).expect("Failed to send kill signal");
-                }
-            });
+            if let Err(err) = start_backend() {
+                println!("Failed to start backend: {err}");
+                *app.state::<AppState>().backend_error.lock().unwrap() = Some(err.to_string());
+            }
 
             Ok(())
         })
-        // Tell the child process to shutdown when app exits
-        .invoke_handler(tauri::generate_handler![open_finder, open_powerpoint, get_backend_error])
+        .invoke_handler(tauri::generate_handler![
+            open_finder,
+            open_powerpoint,
+            get_backend_error
+        ])
         .run(tauri::generate_context!())
+        .context("Error while running app")
 }
