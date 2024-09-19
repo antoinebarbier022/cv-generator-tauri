5493a771a1af84b8324e0048328f071ace4ec2be
Antoine Barbier <antoine.c.barbier@frog.co>
2024-09-19 05:28:18 +0200

    refactor: navigation to welcome page if user name is not defined

--- src/hooks/useRedirectToWelcomePage.tsx
+++ src/hooks/useRedirectToWelcomePage.tsx
@@ -1,18 +1,14 @@
-import { StorageService } from '@/services/StorageService'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useFormCV } from './useFormCV'
 
 export const useRedirectToWelcomePage = () => {
   const navigate = useNavigate()
+  const {formValues} = useFormCV()
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await StorageService.getContentData() // Supposons que `data` soit une promesse ou que tu attendes un appel asynchrone pour obtenir `data`
-
-      if (result?.firstname === '' && result?.lastname === '') {
-        navigate('/welcome')
-      }
+    if(formValues.firstname === '' && formValues.lastname === ''){
+      navigate('/welcome')
+      console.info("Navigate to '/welcome' page.")
     }
-
-    fetchData()
   }, [])
 }
