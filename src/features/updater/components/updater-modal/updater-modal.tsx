61e64337ac0e664a4dccea820c139f99ef2018e5
Antoine Barbier <antoine.c.barbier@frog.co>
2024-09-25 18:20:53 +0200

    chore: add storybook and vitest (#9)
    * chore: add storybook and updater stories
    
    * chore(tests): add tests for utils

---
+++ src/features/updater/components/updater-modal/updater-modal.tsx
@@ -0,0 +1,198 @@
+import AppIcon from '@/assets/images/icon.svg?react'
+import UpdateErrorSVG from '@/assets/images/update-error.svg?react'
+
+import i18n from '@/configs/i18n.config'
+import { Card, LinearProgress, Stack } from '@mui/joy'
+import { partial } from 'filesize'
+import { useTranslation } from 'react-i18next'
+import { AppUpdaterStatus } from '../../types/updater.types'
+import { RealeaseNoteMarkdown } from '../release-note-markdown'
+import { UpdaterModalLayout } from '../updater-modal-layout/updater-modal-layout'
+
+interface Props {
+  open: boolean
+  status: AppUpdaterStatus
+  onClose?: () => void
+  onCancel?: () => void
+  onConfirm?: () => void
+  optionalContent: {
+    currentVersion?: string
+    nextVersion?: string
+    downloadedLength?: number
+    totalUpdateLength?: number
+    releaseNote?: string
+  }
+}
+
+export const UpdaterModal = (props: Props) => {
+  const { t } = useTranslation()
+
+  const { currentVersion, nextVersion, releaseNote, downloadedLength, totalUpdateLength } =
+    props.optionalContent
+
+  if (props.optionalContent && props.status === AppUpdaterStatus.NO_UPDATE_AVAILABLE) {
+    return (
+      <UpdaterModalLayout
+        open={props.open}
+        onClose={props.onClose}
+        onCancel={props.onCancel}
+        config={{
+          icon: <AppIcon />,
+          title: t('updater.no-update.title'),
+          description: t('updater.no-update.description', {
+            currentVersion: currentVersion ?? ''
+          }),
+          cancelLabel: t('updater.no-update.cancel')
+        }}
+      />
+    )
+  }
+
+  if (props.status === AppUpdaterStatus.DOWNLOADING_UPDATE) {
+    const isProgression = Boolean(downloadedLength && downloadedLength >= 1)
+    const progression =
+      downloadedLength && totalUpdateLength ? (downloadedLength / totalUpdateLength) * 100 : 0
+
+    const size = partial({ standard: 'jedec', locale: i18n.languages[0], pad: true })
+    const getTitle = () => {
+      if (totalUpdateLength === null || totalUpdateLength === 0) {
+        return t('updater.downloading-update.waiting')
+      }
+      if (!totalUpdateLength) {
+        return 'error'
+      }
+      if (props.status === AppUpdaterStatus.DOWNLOADING_UPDATE) {
+        return `${size(downloadedLength ?? 0)} / ${size(totalUpdateLength)}`
+      }
+      return t('updater.downloading-update.waiting')
+    }
+    return (
+      <UpdaterModalLayout
+        open={props.open}
+        onCancel={props.onClose}
+        config={{
+          icon: <AppIcon />,
+          title: getTitle(),
+          cancelLabel: t('updater.downloading-update.cancel')
+        }}
+      >
+        <Stack pt={2}>
+          <LinearProgress
+            determinate={isProgression}
+            value={isProgression ? progression : undefined}
+          />
+        </Stack>
+      </UpdaterModalLayout>
+    )
+  }
+
+  if (props.status === AppUpdaterStatus.UPDATE_DOWNLOADED) {
+    return (
+      <>
+        <UpdaterModalLayout
+          open={props.open}
+          onConfirm={props.onConfirm}
+          onCancel={props.onClose}
+          onClose={props.onClose}
+          config={{
+            icon: <AppIcon />,
+            title: t('updater.update-downloaded.title', { version: nextVersion }),
+            confirmLabel: t('updater.update-downloaded.confirm'),
+            cancelLabel: t('updater.update-downloaded.cancel')
+          }}
+        >
+          <Stack pt={2}>
+            <LinearProgress determinate value={100} />
+          </Stack>
+        </UpdaterModalLayout>
+      </>
+    )
+  }
+
+  if (props.status === AppUpdaterStatus.DOWNLOAD_FAILED) {
+    const progression =
+      downloadedLength && totalUpdateLength ? (downloadedLength / totalUpdateLength) * 100 : 0
+    return (
+      <>
+        <UpdaterModalLayout
+          open={props.open}
+          onCancel={props.onCancel}
+          onConfirm={props.onConfirm}
+          config={{
+            icon: <UpdateErrorSVG />,
+            title: t('updater.download-failed.title'),
+            description: nextVersion && `v${nextVersion}`,
+            kind: 'error',
+            confirmLabel: t('updater.download-failed.confirm')
+          }}
+        >
+          <Stack pt={2}>
+            <LinearProgress color="danger" determinate value={progression} />
+          </Stack>
+        </UpdaterModalLayout>
+      </>
+    )
+  }
+
+  if (props.status === AppUpdaterStatus.UPDATE_AVAILABLE) {
+    return (
+      <UpdaterModalLayout
+        open={props.open}
+        onClose={props.onClose}
+        onCancel={props.onCancel}
+        onConfirm={props.onConfirm}
+        config={{
+          size: 'md',
+          icon: <AppIcon />,
+          title: t('updater.update-available.title'),
+          description: t('updater.update-available.description', {
+            newVersion: nextVersion,
+            currentVersion: currentVersion
+          }),
+          confirmLabel: t('updater.update-available.confirm'),
+          cancelLabel: t('updater.update-available.cancel')
+        }}
+      >
+        {releaseNote && (
+          <Card variant="soft" sx={{ mt: 1, backgroundColor: 'neutral.50' }}>
+            <RealeaseNoteMarkdown content={releaseNote} />
+          </Card>
+        )}
+      </UpdaterModalLayout>
+    )
+  }
+  if (props.status === AppUpdaterStatus.CHECKING_FOR_UPDATES) {
+    return (
+      <UpdaterModalLayout
+        open={props.open}
+        onClose={props.onClose}
+        config={{
+          icon: <AppIcon />,
+          title: t('updater.checking-for-updates.title')
+        }}
+      >
+        <Stack pt={2}>
+          <LinearProgress size="lg" />
+        </Stack>
+      </UpdaterModalLayout>
+    )
+  }
+
+  if (props.status === AppUpdaterStatus.CHECK_ERROR) {
+    return (
+      <UpdaterModalLayout
+        open={props.open}
+        onCancel={props.onCancel}
+        onConfirm={props.onConfirm}
+        config={{
+          kind: props.status === AppUpdaterStatus.CHECK_ERROR ? 'error' : 'info',
+          icon: <UpdateErrorSVG />,
+          title: t('updater.check-error.title'),
+          confirmLabel: t('updater.check-error.confirm')
+        }}
+      ></UpdaterModalLayout>
+    )
+  }
+
+  return <></>
+}
--- src/features/updater/containers/updater-container.tsx
+++ src/features/updater/containers/updater-container.tsx
@@ -1,27 +1,13 @@
-import AppIcon from '@/assets/images/icon.svg?react'
-import UpdateErrorSVG from '@/assets/images/update-error.svg?react'
-
-import i18n from '@/configs/i18n.config'
-import { Card, LinearProgress, Stack } from '@mui/joy'
-import { partial } from 'filesize'
-import { useTranslation } from 'react-i18next'
-import { RealeaseNoteMarkdown } from '../components/release-note-markdown'
-import { UpdaterModal } from '../components/updater-modal'
+import { UpdaterModal } from '../components/updater-modal/updater-modal'
 import { useAppUpdater } from '../hooks/useAppUpdater'
 import { AppUpdaterStatus } from '../types/updater.types'
 
 interface Props {
   open: boolean
   onClose: () => void
-  config?: {
-    title: string
-    description: string
-  }
 }
 
 export const UpdaterContainer = (props: Props) => {
-  const { t } = useTranslation()
-
   const {
     status,
     update,
@@ -50,161 +36,100 @@ export const UpdaterContainer = (props: Props) => {
   if (currentVersion && status === AppUpdaterStatus.NO_UPDATE_AVAILABLE) {
     return (
       <UpdaterModal
+        status={AppUpdaterStatus.NO_UPDATE_AVAILABLE}
         open={props.open}
         onClose={handleClose}
         onCancel={handleCancel}
-        config={{
-          icon: <AppIcon />,
-          title: t('updater.no-update.title'),
-          description: t('updater.no-update.description', {
-            currentVersion: currentVersion ?? ''
-          }),
-          cancelLabel: t('updater.no-update.cancel')
+        optionalContent={{
+          currentVersion
         }}
       />
     )
   }
 
   if (update && downloadedLength !== null && status === AppUpdaterStatus.DOWNLOADING_UPDATE) {
-    const isProgression = Boolean(downloadedLength && downloadedLength >= 1)
-    const progression =
-      downloadedLength && totalUpdateLength ? (downloadedLength / totalUpdateLength) * 100 : 0
-
-    const size = partial({ standard: 'jedec', locale: i18n.languages[0], pad: true })
-    const getTitle = () => {
-      if (totalUpdateLength === null || totalUpdateLength === 0) {
-        return t('updater.downloading-update.waiting')
-      }
-      if (status === AppUpdaterStatus.DOWNLOADING_UPDATE) {
-        return `${size(downloadedLength ?? 0)} / ${size(totalUpdateLength)}`
-      }
-      return t('updater.downloading-update.waiting')
-    }
     return (
       <UpdaterModal
+        status={AppUpdaterStatus.UPDATE_DOWNLOADED}
         open={props.open}
         onCancel={handleClose}
-        config={{
-          icon: <AppIcon />,
-          title: getTitle(),
-          cancelLabel: t('updater.downloading-update.cancel')
+        optionalContent={{
+          nextVersion: update?.version,
+          downloadedLength: downloadedLength ?? undefined,
+          totalUpdateLength: totalUpdateLength ?? undefined
         }}
-      >
-        <Stack pt={2}>
-          <LinearProgress
-            determinate={isProgression}
-            value={isProgression ? progression : undefined}
-          />
-        </Stack>
-      </UpdaterModal>
+      />
     )
   }
 
   if (status === AppUpdaterStatus.UPDATE_DOWNLOADED) {
     return (
-      <>
-        <UpdaterModal
-          open={props.open}
-          onConfirm={relaunch}
-          onCancel={handleClose}
-          onClose={handleClose}
-          config={{
-            icon: <AppIcon />,
-            title: t('updater.update-downloaded.title', { version: update?.version }),
-            confirmLabel: t('updater.update-downloaded.confirm'),
-            cancelLabel: t('updater.update-downloaded.cancel')
-          }}
-        >
-          <Stack pt={2}>
-            <LinearProgress determinate value={100} />
-          </Stack>
-        </UpdaterModal>
-      </>
+      <UpdaterModal
+        status={AppUpdaterStatus.UPDATE_DOWNLOADED}
+        open={props.open}
+        onClose={handleClose}
+        onCancel={handleClose}
+        onConfirm={relaunch}
+        optionalContent={{
+          nextVersion: update?.version
+        }}
+      />
     )
   }
 
   if (status === AppUpdaterStatus.DOWNLOAD_FAILED) {
-    const progression =
-      downloadedLength && totalUpdateLength ? (downloadedLength / totalUpdateLength) * 100 : 0
     return (
-      <>
-        <UpdaterModal
-          open={props.open}
-          onCancel={handleCancel}
-          onConfirm={handleDownloadAndInstall}
-          config={{
-            icon: <UpdateErrorSVG />,
-            title: t('updater.download-failed.title'),
-            description: update?.version && `v${update?.version}`,
-            kind: 'error',
-            confirmLabel: t('updater.download-failed.confirm')
-          }}
-        >
-          <Stack pt={2}>
-            <LinearProgress color="danger" determinate value={progression} />
-          </Stack>
-        </UpdaterModal>
-      </>
+      <UpdaterModal
+        status={AppUpdaterStatus.DOWNLOAD_FAILED}
+        open={props.open}
+        onCancel={handleCancel}
+        onConfirm={handleDownloadAndInstall}
+        optionalContent={{
+          nextVersion: update?.version,
+          downloadedLength: downloadedLength ?? undefined,
+          totalUpdateLength: totalUpdateLength ?? undefined
+        }}
+      />
     )
   }
 
   if (update && status === AppUpdaterStatus.UPDATE_AVAILABLE) {
     return (
       <UpdaterModal
+        status={AppUpdaterStatus.UPDATE_AVAILABLE}
         open={props.open}
         onClose={handleClose}
         onCancel={handleCancel}
         onConfirm={handleDownloadAndInstall}
-        config={{
-          size: 'md',
-          icon: <AppIcon />,
-          title: t('updater.update-available.title'),
-          description: t('updater.update-available.description', {
-            newVersion: update.version,
-            currentVersion: update.currentVersion
-          }),
-          confirmLabel: t('updater.update-available.confirm'),
-          cancelLabel: t('updater.update-available.cancel')
+        optionalContent={{
+          nextVersion: update?.version,
+          currentVersion: update?.currentVersion,
+          releaseNote: update?.body
         }}
-      >
-        {update.body && (
-          <Card variant="soft" sx={{ mt: 1, backgroundColor: 'neutral.50' }}>
-            <RealeaseNoteMarkdown content={update.body} />
-          </Card>
-        )}
-      </UpdaterModal>
+      />
     )
   }
+
   if (status === AppUpdaterStatus.CHECKING_FOR_UPDATES) {
     return (
       <UpdaterModal
+        status={AppUpdaterStatus.CHECKING_FOR_UPDATES}
         open={props.open}
         onClose={handleClose}
-        config={{
-          icon: <AppIcon />,
-          title: t('updater.checking-for-updates.title')
-        }}
-      >
-        <Stack pt={2}>
-          <LinearProgress size="lg" />
-        </Stack>
-      </UpdaterModal>
+        optionalContent={{}}
+      />
     )
   }
 
   if (status === AppUpdaterStatus.CHECK_ERROR) {
     return (
       <UpdaterModal
+        status={AppUpdaterStatus.CHECK_ERROR}
         open={props.open}
         onCancel={handleCancel}
         onConfirm={checkForUpdates}
-        config={{
-          kind: status === AppUpdaterStatus.CHECK_ERROR ? 'error' : 'info',
-          icon: <UpdateErrorSVG />,
-          title: t('updater.check-error.title'),
-          confirmLabel: t('updater.check-error.confirm')
-        }}
-      ></UpdaterModal>
+        optionalContent={{}}
+      />
     )
   }
 
